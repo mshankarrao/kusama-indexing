0f2a19c982e3b5781ab0e44b20d88ea0fcca0e2f
Shankar Mata <mshankarrao1970@gmail.com>
2021-09-24 10:26:43 -0400

    adding loggers for tracking council output

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -12,7 +12,7 @@ export async function handleVoting(event: SubstrateEvent): Promise<void> {
     const number_voted_yes = event.event.data[3];
     const number_voted_no = event.event.data[4];
 
-    // logger.info('new Council vote' + JSON.stringify(event.event.data));
+    logger.info('new Council vote' + JSON.stringify(event.event.data));
 
     const councilVote = new StarterEntity(
         `${event.block.block.header.number.toNumber()}-${event.idx}`,
